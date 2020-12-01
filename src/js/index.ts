1e8951bc7d1a37bfeaabdb73d7d483ff4c2f1d07
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-12-01 12:44:18 +0100

    Har lavet to Get funktioner

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,49 @@
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+interface ICoronaTest {
+    testId: number
+    machineName: string
+    temperature: number
+    location: string
+    date: string
+    time: string
+} 
+
+let baseUrl: string = "https://coronatest.azurewebsites.net/api/CoronaTests"
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        tests: []
+
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+        getAllTests() {
+            this.helperGetAndShow(baseUrl)
+        },
+        helperGetAndShow(url: string) {
+            axios.get<ICoronaTest[]>(url)
+                .then((response: AxiosResponse<ICoronaTest[]>) => {
+                    this.tests = response.data
+                })
+                .catch((error: AxiosError) => {
+                    //this.message = error.message
+                    alert(error.message) // https://www.w3schools.com/js/js_popup.asp
+                })
+        },
+        getAllWithHighTemperature(temperature: number) {
+            let url = baseUrl + "/temperature/" + temperature
+            this.helperGetAndShow(url)
+        },
+        showPercentage() {
+
         }
     }
+
 })
