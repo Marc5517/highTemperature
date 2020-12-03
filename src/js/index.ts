fe7907904db74990c141be1cd409f7c984f0bd11
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-12-03 13:42:21 +0100

    Har lavet en metode, fået antal og procent

--- src/js/index.ts
+++ src/js/index.ts
@@ -10,7 +10,7 @@ interface ICoronaTest {
     location: string
     date: string
     time: string
-} 
+}
 
 let baseUrl: string = "https://coronatest.azurewebsites.net/api/CoronaTests"
 
@@ -20,25 +20,29 @@ new Vue({
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        tests: []
+        tests: [],
+        highTemp: []
 
     },
     methods: {
         getAllTests() {
-            this.helperGetAndShow(baseUrl)
+            this.helperGetAndShow(baseUrl, "all")
         },
-        helperGetAndShow(url: string) {
+        helperGetAndShow(url: string, type: string) {
             axios.get<ICoronaTest[]>(url)
                 .then((response: AxiosResponse<ICoronaTest[]>) => {
-                    this.tests = response.data
+                    if (type == "all")
+                        this.tests = response.data
+                    else
+                        this.highTemp = response.data
                 })
                 .catch((error: AxiosError) => {
                     //this.message = error.message
                     alert(error.message) // https://www.w3schools.com/js/js_popup.asp
                 })
         },
-        getAllWithHighTemperature(temperature: number) {
-            let url = baseUrl + "/temperature/" + temperature
+        getAllWithHighTemperature() {
+            let url = baseUrl + "/temperature"
             this.helperGetAndShow(url)
         },
         showPercentage() {
